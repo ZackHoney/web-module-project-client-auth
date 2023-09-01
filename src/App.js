ab2a5f7b0148cf0aa4819b0fd5c78cf1a66d8832
Zachary Honey <zhoney466@gmail.com>
2023-08-31 21:15:00 -0700

    Started Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+const Login = () => {
+  return <h2>Login</h2>
+}
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Routes>
+      <Route path='/'>
+        <Login />
+      </Route>
+      </Routes>
     </div>
+    </Router>
   );
+
 }
 
 export default App;
