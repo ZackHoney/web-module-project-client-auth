fdf0184dde19bc9289021cd9bbe314cd57ac4500
Zachary Honey <zhoney466@gmail.com>
2023-09-04 12:49:59 -0700

    Finished Login component items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Login from './Components/Login';
 import FriendsList from './Components/FriendsList';
@@ -12,6 +12,14 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <header>
+          <h2>Friends Database</h2>
+        <Link className='link'to='login'>Login</Link> 
+        <Link className='link'to='friends'>My Friends List</Link>
+        <Link className='link'to='friends/add'>Add A Friend</Link>
+        <Link className='link'to='/'>Logout</Link>
+
+        </header>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/login' element={<Login />} />
